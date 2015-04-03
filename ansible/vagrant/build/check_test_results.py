801e3d9a2ab0624516a6a484f125b8952ebec082
Rafał Krysiak <rafal@zato.io>
2015-04-03 16:44:36 +0200

    Moved exception handler outside of 'check_content' function; vm subdirectories are now sorted; added a function checking specified test results.

--- ansible/vagrant/build/check_test_results.py
+++ ansible/vagrant/build/check_test_results.py
@@ -9,8 +9,19 @@ import re
 logging.basicConfig(level=logging.INFO)
 
 current_dir = os.getcwd()
-vm_dir = os.path.join(current_dir, 'vm')
-virtual_machines = os.listdir(vm_dir)
+main_vm_dir = os.path.join(current_dir, 'vm')
+virtual_machines = sorted(os.listdir(main_vm_dir))
+test_passed = []
+
+
+def check_result(result):
+
+    if result == "ZATO_OK":
+        test_passed.append(True)
+    elif result is True:
+        test_passed.append(True)
+    else:
+        test_passed.append(False)
 
 
 def check_content(file):
@@ -19,37 +30,45 @@ def check_content(file):
     pattern_info = re.compile(r'info_*')
     pattern_ping = re.compile(r'ping_*')
 
-    try:
-        content = json.load(output_file)
+    content = json.load(output_file)
 
-        if re.search(pattern_info, file):
-            details = {
-                'name': content['component_details']['component'],
-                'running': content['component_running'],
-                'version': content['component_details']['version'],
-            }
-            logging.info("Component:")
+    if re.search(pattern_info, file):
+        details = {
+            'name': content['component_details']['component'],
+            'running': content['component_running'],
+            'version': content['component_details']['version'],
+        }
+        logging.info(" Component:")
 
-            for key, value in details.iteritems():
-                logging.info("%s%s: %s" % ('\t', key, value))
-            print '\n'
+        for key, value in details.iteritems():
+            logging.info(" - %s: %s" % (key, value))
+            check_result(value)
+        print '\n'
 
-        elif re.search(pattern_ping, file):
-            details = {
-                'result': content['zato_env']['result']
-            }
-            logging.info("%s%s: %s\n" % ('\t', 'ping', details['result']))
-
-    except ValueError:
-        logging.warning("Content of this file is not json.\n")
+    elif re.search(pattern_ping, file):
+        details = {
+            'result': content['zato_env']['result']
+        }
+        result = details['result']
+        logging.info(" %s: %s\n" % ('ping', result))
+        check_result(result)
 
 
 for directory in virtual_machines:
-    logging.info(40 * "=")
-    logging.info("Checking test results for %s", directory)
-    test_directory = os.path.join(vm_dir, directory, 'tests')
+    logging.info(" " + 45 * "=")
+    logging.info(" Checking test results for %s", directory)
+    test_directory = os.path.join(main_vm_dir, directory, 'tests')
     for root, dirs, files in os.walk(test_directory):
-        for item in files:
-            logging.info("Checking %s now", item)
-            item = os.path.join(test_directory, item)
-            check_content(item)
+        for filename in files:
+            logging.info(" Checking %s now", filename)
+            filename = os.path.join(test_directory, filename)
+            try:
+                check_content(filename)
+            except ValueError:
+                logging.info(" Content of this file is not json. Skipping.\n")
+
+if test_passed.count(False) == 0:
+    logging.info("All tests passed")
+else:
+    logging.info("%s" %
+        " Some tests have failed. Refer to test output files to check what has gone wrong.")
