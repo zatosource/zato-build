8f1db2b8e399534c54cd6f0c5a6d258a0154bc29
Rafał Krysiak <rafal@zato.io>
2015-06-18 13:33:03 +0200

    Changed certificate file names.

--- docker/components/gencert.sh
+++ docker/components/gencert.sh
@@ -9,40 +9,40 @@ else
     exit
 fi
 
-#create CA
+# Create CA
 echo "Generating CA..."
-openssl genrsa -out zato.ca.key.pem 2048
-openssl req -new -x509 -days 3650 -extensions v3_ca -subj "/C=EU/ST=Zatoland/L=Zato City/O=Zato/CN=Zato Dev CA" -key zato.ca.key.pem -out ca_cert.pem
+openssl genrsa -out sample.ca.key.pem 2048
+openssl req -new -x509 -days 3650 -extensions v3_ca -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Sample Dev CA" -key sample.ca.key.pem -out ca_cert.pem
 echo ""
 
-#create load balancer certs
+# Create load balancer certs
 echo "Generating Load Balancer certs..."
-openssl genrsa -out zato.load_balancer.key.pem 2048
-openssl rsa -in zato.load_balancer.key.pem -pubout -out zato.load_balancer.key.pub.pem
-openssl req -new -key zato.load_balancer.key.pem -out zato.load_balancer.req.csr -subj "/C=EU/ST=Zatoland/L=Zato City/O=Zato/CN=Zato Dev Load Balancer"
-openssl x509 -req -days 365 -in zato.load_balancer.req.csr -CA ca_cert.pem -CAkey zato.ca.key.pem -CAcreateserial -out zato.load_balancer.cert.pem
-rm -f zato.load_balancer.req.csr
+openssl genrsa -out sample.load_balancer.key.pem 2048
+openssl rsa -in sample.load_balancer.key.pem -pubout -out sample.load_balancer.key.pub.pem
+openssl req -new -key sample.load_balancer.key.pem -out sample.load_balancer.req.csr -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Sample Dev Load Balancer"
+openssl x509 -req -days 365 -in sample.load_balancer.req.csr -CA ca_cert.pem -CAkey sample.ca.key.pem -CAcreateserial -out sample.load_balancer.cert.pem
+rm -f sample.load_balancer.req.csr
 echo ""
 
-#create web admin certs
+# Create web admin certs
 echo "Generating Web Admin certs..."
-openssl genrsa -out zato.web_admin.key.pem 2048
-openssl rsa -in zato.web_admin.key.pem -pubout -out zato.web_admin.key.pub.pem
-openssl req -new -key zato.web_admin.key.pem -out zato.web_admin.req.csr -subj "/C=EU/ST=Zatoland/L=Zato City/O=Zato/CN=Zato Dev Web Admin"
-openssl x509 -req -days 365 -in zato.web_admin.req.csr -CA ca_cert.pem -CAkey zato.ca.key.pem -CAcreateserial -out zato.web_admin.cert.pem
-rm -f zato.web_admin.req.csr
+openssl genrsa -out sample.web_admin.key.pem 2048
+openssl rsa -in sample.web_admin.key.pem -pubout -out sample.web_admin.key.pub.pem
+openssl req -new -key sample.web_admin.key.pem -out sample.web_admin.req.csr -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Sample Dev Web Admin"
+openssl x509 -req -days 365 -in sample.web_admin.req.csr -CA ca_cert.pem -CAkey sample.ca.key.pem -CAcreateserial -out sample.web_admin.cert.pem
+rm -f sample.web_admin.req.csr
 echo ""
 
-#create servers certs
+# Create servers certs
 i=1
 while [ $i -le $HOWMANYSERVERS ]
 do
 echo "Generating Server $i certs..."
-openssl genrsa -out zato.server$i.key.pem 2048
-openssl rsa -in zato.server$i.key.pem -pubout -out zato.server$i.key.pub.pem
-openssl req -new -key zato.server$i.key.pem -out zato.server$i.req.csr -subj "/C=EU/ST=Zatoland/L=Zato City/O=Zato/CN=Zato Dev Server $i"
-openssl x509 -req -days 365 -in zato.server$i.req.csr -CA ca_cert.pem -CAkey zato.ca.key.pem -CAcreateserial -out zato.server$i.cert.pem
-rm -f zato.server$i.req.csr
+openssl genrsa -out sample.server$i.key.pem 2048
+openssl rsa -in sample.server$i.key.pem -pubout -out sample.server$i.key.pub.pem
+openssl req -new -key sample.server$i.key.pem -out sample.server$i.req.csr -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Sample Dev Server $i"
+openssl x509 -req -days 365 -in sample.server$i.req.csr -CA ca_cert.pem -CAkey sample.ca.key.pem -CAcreateserial -out sample.server$i.cert.pem
+rm -f sample.server$i.req.csr
 echo ""
 i=$[i+1]
 done
