5f8e79efcb57f84107760ebf0f29193c4f566f6c
Rafał Krysiak <rafal@zato.io>
2015-10-27 19:56:00 +0100

    Refactored the code. The script is fully functional now.

--- ansible/vagrant/build/files/zato-bst-install.sh
+++ ansible/vagrant/build/files/zato-bst-install.sh
@@ -1,53 +1,69 @@
 #!/bin/bash
 
-zato stop /opt/zato/env/qs-1/server1
-zato stop /opt/zato/env/qs-1/server2
+ZATO_ROOT=/opt/zato
+BST_ROOT=zato-labs/bst
+ZATO_ENV=env/qs-1
 
-if [ ! -d /opt/zato/zato-labs ]
+cd /opt/zato
+ZATO_VERSION=`ls | sort -n | tail -1`
+
+zato stop $ZATO_ROOT/$ZATO_ENV/server1
+zato stop $ZATO_ROOT/$ZATO_ENV/server2
+
+if [ ! -d $ZATO_ROOT/zato-labs ]
 then
     git clone https://github.com/zatosource/zato-labs.git
 fi
 
-cd /opt/zato/zato-labs/bst
+cd $ZATO_ROOT/$BST_ROOT
 make
 
-ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/blockdiag /opt/zato/2.0.7/zato_extra_paths
-ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/funcparserlib /opt/zato/2.0.7/zato_extra_paths
-ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/PIL /opt/zato/2.0.7/zato_extra_paths
-ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/webcolors.py /opt/zato/2.0.7/zato_extra_paths
-ln -s /opt/zato/zato-labs/bst/src/zato/bst/__init__.py /opt/zato/2.0.7/zato_extra_paths/zato_bst.py
+ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/blockdiag \
+      $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths
+ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/funcparserlib \
+      $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths
+ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/PIL \
+      $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths
+ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/webcolors.py \
+      $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths
+ln -s $ZATO_ROOT/$BST_ROOT/src/zato/bst/__init__.py \
+      $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths/zato_bst.py
 
-mkdir -p /opt/zato/env/qs-1/server1/config/repo/proc/bst
-mkdir -p /opt/zato/env/qs-1/server2/config/repo/proc/bst
+mkdir -p $ZATO_ROOT/$ZATO_ENV/server1/config/repo/proc/bst
+mkdir -p $ZATO_ROOT/$ZATO_ENV/server2/config/repo/proc/bst
 
-cp /opt/zato/zato-labs/bst/sample.txt /opt/zato/env/qs-1/server1/config/repo/proc/bst
-cp /opt/zato/zato-labs/bst/sample.txt /opt/zato/env/qs-1/server2/config/repo/proc/bst
+cp $ZATO_ROOT/$BST_ROOT/sample.txt $ZATO_ROOT/$ZATO_ENV/server1/config/repo/proc/bst
+cp $ZATO_ROOT/$BST_ROOT/sample.txt $ZATO_ROOT/$ZATO_ENV/server2/config/repo/proc/bst
 
-zato start /opt/zato/env/qs-1/server1
-sleep 60
-zato start /opt/zato/env/qs-1/server2
-sleep 60
+zato start $ZATO_ROOT/$ZATO_ENV/server1
+sleep 10
+zato start $ZATO_ROOT/$ZATO_ENV/server2
+sleep 10
 
-cp /opt/zato/zato-labs/bst/src/zato/bst/services.py /opt/zato/env/qs-1/server1/pickup-dir
-sleep 30
-cp /opt/zato/zato-labs/bst/src/zato/bst/services.py /opt/zato/env/qs-1/server2/pickup-dir
-sleep 30
+cp $ZATO_ROOT/$BST_ROOT/src/zato/bst/services.py $ZATO_ROOT/$ZATO_ENV/server1/pickup-dir
+sleep 10
+cp $ZATO_ROOT/$BST_ROOT/src/zato/bst/services.py $ZATO_ROOT/$ZATO_ENV/server2/pickup-dir
+sleep 10
 
 sed -i '/startup_services_first_worker/a \
 labs.proc.bst.definition.startup-setup=' \
-/opt/zato/env/qs-1/server1/config/repo/server.conf
+$ZATO_ROOT/$ZATO_ENV/server1/config/repo/server.conf
 
 sed -i '/startup_services_first_worker/a \
 labs.proc.bst.definition.startup-setup=' \
-/opt/zato/env/qs-1/server2/config/repo/server.conf
+$ZATO_ROOT/$ZATO_ENV/server2/config/repo/server.conf
 
 # Generate a random password...
-uuidgen > /opt/zato/random_password.txt
+uuidgen > $ZATO_ROOT/random_password.txt
 # ...and replace a variable with the password.
-sed -i 's/$BST_PASSWORD/'\"$(cat /opt/zato/random_password.txt)\"'/g' \
-       /opt/zato/zato-labs/bst/bst-enmasse.json
+sed -i 's/$BST_PASSWORD/'\"$(cat $ZATO_ROOT/random_password.txt)\"'/g' \
+       $ZATO_ROOT/$BST_ROOT/bst-enmasse.json
 
-zato enmasse /opt/zato/env/qs-1/server1/ --input /opt/zato/zato-labs/bst/bst-enmasse.json --import --replace-odb-objects
-zato enmasse /opt/zato/env/qs-1/server2/ --input /opt/zato/zato-labs/bst/bst-enmasse.json --import --replace-odb-objects
+zato enmasse $ZATO_ROOT/$ZATO_ENV/server1/ \
+     --input $ZATO_ROOT/$BST_ROOT/bst-enmasse.json \
+     --import --replace-odb-objects
+zato enmasse $ZATO_ROOT/$ZATO_ENV/server2/ \
+     --input $ZATO_ROOT/$BST_ROOT/bst-enmasse.json \
+     --import --replace-odb-objects
 
-curl http://bst:`cat /opt/zato/random_password.txt`@localhost:11223/bst/get-definition-list
+curl http://bst:`cat $ZATO_ROOT/random_password.txt`@localhost:11223/bst/get-definition-list
