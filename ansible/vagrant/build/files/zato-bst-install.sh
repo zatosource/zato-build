544d85c394bd70f65f61f49f6851bc1428990fbe
Rafał Krysiak <rafal@zato.io>
2015-10-27 17:49:05 +0100

    Commiting a first working version of BST installer script.

--- ansible/vagrant/build/files/zato-bst-install.sh
+++ ansible/vagrant/build/files/zato-bst-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-zato stop ./env/qs-1/server1
-zato stop ./env/qs-1/server2
+zato stop /opt/zato/env/qs-1/server1
+zato stop /opt/zato/env/qs-1/server2
 
 if [ ! -d /opt/zato/zato-labs ]
 then
@@ -11,24 +11,27 @@ fi
 cd /opt/zato/zato-labs/bst
 make
 
-ln -s $PWD/bst-env/lib/python2.7/site-packages/blockdiag /opt/zato/2.0.7/zato_extra_paths
-ln -s $PWD/bst-env/lib/python2.7/site-packages/funcparserlib /opt/zato/2.0.7/zato_extra_paths
-ln -s $PWD/bst-env/lib/python2.7/site-packages/PIL /opt/zato/2.0.7/zato_extra_paths
-ln -s $PWD/bst-env/lib/python2.7/site-packages/webcolors.py /opt/zato/2.0.7/zato_extra_paths
-ln -s $PWD/src/zato/bst/__init__.py /opt/zato/2.0.7/zato_extra_paths/zato_bst.py
+ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/blockdiag /opt/zato/2.0.7/zato_extra_paths
+ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/funcparserlib /opt/zato/2.0.7/zato_extra_paths
+ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/PIL /opt/zato/2.0.7/zato_extra_paths
+ln -s /opt/zato/zato-labs/bst/bst-env/lib/python2.7/site-packages/webcolors.py /opt/zato/2.0.7/zato_extra_paths
+ln -s /opt/zato/zato-labs/bst/src/zato/bst/__init__.py /opt/zato/2.0.7/zato_extra_paths/zato_bst.py
 
-cd /opt/zato/
-mkdir -p ./env/qs-1/server1/config/repo/proc/bst
-mkdir -p ./env/qs-1/server2/config/repo/proc/bst
+mkdir -p /opt/zato/env/qs-1/server1/config/repo/proc/bst
+mkdir -p /opt/zato/env/qs-1/server2/config/repo/proc/bst
 
 cp /opt/zato/zato-labs/bst/sample.txt /opt/zato/env/qs-1/server1/config/repo/proc/bst
 cp /opt/zato/zato-labs/bst/sample.txt /opt/zato/env/qs-1/server2/config/repo/proc/bst
 
-zato start ./env/qs-1/server1
-zato start ./env/qs-1/server2
+zato start /opt/zato/env/qs-1/server1
+sleep 60
+zato start /opt/zato/env/qs-1/server2
+sleep 60
 
-cp /opt/zato/zato-labs/bst/src/zato/bst/services.py ./env/qs-1/server1/pickup-dir
-cp /opt/zato/zato-labs/bst/src/zato/bst/services.py ./env/qs-1/server2/pickup-dir
+cp /opt/zato/zato-labs/bst/src/zato/bst/services.py /opt/zato/env/qs-1/server1/pickup-dir
+sleep 30
+cp /opt/zato/zato-labs/bst/src/zato/bst/services.py /opt/zato/env/qs-1/server2/pickup-dir
+sleep 30
 
 sed -i '/startup_services_first_worker/a \
 labs.proc.bst.definition.startup-setup=' \
@@ -41,7 +44,7 @@ labs.proc.bst.definition.startup-setup=' \
 # Generate a random password...
 uuidgen > /opt/zato/random_password.txt
 # ...and replace a variable with the password.
-sed -i 's/$BST_PASSWORD/'"$(cat /opt/zato/random_password.txt)"'/g' \
+sed -i 's/$BST_PASSWORD/'\"$(cat /opt/zato/random_password.txt)\"'/g' \
        /opt/zato/zato-labs/bst/bst-enmasse.json
 
 zato enmasse /opt/zato/env/qs-1/server1/ --input /opt/zato/zato-labs/bst/bst-enmasse.json --import --replace-odb-objects
