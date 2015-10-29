dcb664fa71eccd4ca43f87824121b60931910827
Rafał Krysiak <rafal@zato.io>
2015-10-29 19:25:33 +0100

    Added a simple output to the script.

--- ansible/vagrant/build/files/zato-bst-install.sh
+++ ansible/vagrant/build/files/zato-bst-install.sh
@@ -7,6 +7,7 @@ ZATO_ENV=env/qs-1
 cd /opt/zato
 ZATO_VERSION=`ls | sort -n | tail -1`
 
+echo "Stopping Zato server..."
 zato stop $ZATO_ROOT/$ZATO_ENV/server1
 zato stop $ZATO_ROOT/$ZATO_ENV/server2
 
@@ -15,9 +16,11 @@ then
     git clone https://github.com/zatosource/zato-labs.git
 fi
 
+echo "Compiling BST..."
 cd $ZATO_ROOT/$BST_ROOT
 make
 
+echo "Symlinking Python dependencies..."
 ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/blockdiag \
       $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths
 ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/funcparserlib \
@@ -29,36 +32,43 @@ ln -s $ZATO_ROOT/$BST_ROOT/bst-env/lib/python2.7/site-packages/webcolors.py \
 ln -s $ZATO_ROOT/$BST_ROOT/src/zato/bst/__init__.py \
       $ZATO_ROOT/$ZATO_VERSION/zato_extra_paths/zato_bst.py
 
+echo "Preparing a directory to store BST definitions..."
 mkdir -p $ZATO_ROOT/$ZATO_ENV/server1/config/repo/proc/bst
 mkdir -p $ZATO_ROOT/$ZATO_ENV/server2/config/repo/proc/bst
 
+echo "Copying sample BST definitions..."
 cp $ZATO_ROOT/$BST_ROOT/sample.txt $ZATO_ROOT/$ZATO_ENV/server1/config/repo/proc/bst
 cp $ZATO_ROOT/$BST_ROOT/sample.txt $ZATO_ROOT/$ZATO_ENV/server2/config/repo/proc/bst
 
+echo "Starting Zato server..."
 zato start $ZATO_ROOT/$ZATO_ENV/server1
 sleep 10
 zato start $ZATO_ROOT/$ZATO_ENV/server2
 sleep 10
 
+echo "Hot-deploying BST services..."
 cp $ZATO_ROOT/$BST_ROOT/src/zato/bst/services.py $ZATO_ROOT/$ZATO_ENV/server1/pickup-dir
 sleep 10
 cp $ZATO_ROOT/$BST_ROOT/src/zato/bst/services.py $ZATO_ROOT/$ZATO_ENV/server2/pickup-dir
 sleep 10
 
+echo "Reconfiguring Zato server..."
 sed -i '/startup_services_first_worker/a \
-labs.proc.bst.definition.startup-setup=' \
-$ZATO_ROOT/$ZATO_ENV/server1/config/repo/server.conf
+    labs.proc.bst.definition.startup-setup=' \
+    $ZATO_ROOT/$ZATO_ENV/server1/config/repo/server.conf
 
 sed -i '/startup_services_first_worker/a \
-labs.proc.bst.definition.startup-setup=' \
-$ZATO_ROOT/$ZATO_ENV/server2/config/repo/server.conf
+    labs.proc.bst.definition.startup-setup=' \
+    $ZATO_ROOT/$ZATO_ENV/server2/config/repo/server.conf
 
 # Generate a random password...
+echo "Generating a random password and saving it to a file..."
 uuidgen > $ZATO_ROOT/random_password.txt
 # ...and replace a variable with the password.
 sed -i 's/$BST_PASSWORD/'\"$(cat $ZATO_ROOT/random_password.txt)\"'/g' \
        $ZATO_ROOT/$BST_ROOT/bst-enmasse.json
 
+echo "Importing REST channels and their credentials..."
 zato enmasse $ZATO_ROOT/$ZATO_ENV/server1/ \
      --input $ZATO_ROOT/$BST_ROOT/bst-enmasse.json \
      --import --replace-odb-objects
@@ -66,4 +76,5 @@ zato enmasse $ZATO_ROOT/$ZATO_ENV/server2/ \
      --input $ZATO_ROOT/$BST_ROOT/bst-enmasse.json \
      --import --replace-odb-objects
 
+echo "Getting a list of BST deifnitions installed in a cluster..."
 curl http://bst:`cat $ZATO_ROOT/random_password.txt`@localhost:11223/bst/get-definition-list
