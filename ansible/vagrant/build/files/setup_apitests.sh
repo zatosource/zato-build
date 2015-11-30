5b3f5631b0fcab783e3837fd0613a70308e1583e
Rafał Krysiak <rafal@zato.io>
2015-11-30 17:53:26 +0100

    Fixed box URL; replaced the if check with case statement; added verbose logging to playbook execution; added one more playbook to the setup.

--- ansible/vagrant/build/files/setup_apitests.sh
+++ ansible/vagrant/build/files/setup_apitests.sh
@@ -9,12 +9,13 @@ PACKAGE_VERSION=stable
 DISTRIBUTION=ubuntu
 SYSTEM=ubuntu-14.04-64
 REPOSITORY=
+TEST_SUITE=/opt/zato/development/tests/apitests
 
 # Path to vagrant's user private key
 PRIVATE_KEY="$ANSIBLE_ROOT"/vm/"$SYSTEM"/.vagrant/machines/default/virtualbox/private_key
 
 BOX_NAME="trusty64"
-BOX_URL="http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-12.04_chef-provisionerless.box"
+BOX_URL="http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box"
 
 BOX_ON_LIST=`vagrant box list | grep "$BOX_NAME"`
 
@@ -33,30 +34,40 @@ if [ -f "Vagrantfile" ]; then
     vagrant up
     STATUS="$?"
     if [ "$STATUS" == 0 ]; then
-        echo "There already is a Ubuntu box up and running..."
-        echo "Do you want it to be destroyed and a new one build? [y/n]"
-        read ANSWER
-        if [ "$ANSWER" == "y" ]; then
-            echo "Destroying it and building a new one..."
-            cd "$ANSIBLE_ROOT"
-            ansible-playbook clean.yml
-            ansible-playbook prepare_test_box.yml
-        elif [ "$ANSWER" == "n" ]; then
-            echo "Proceeding with apitests..."
-        else
-            echo "Answer should be either "y" or "n""
-            exit 1
-        fi
+        while true
+        do
+            echo "There already is a Ubuntu box up and running..."
+            echo "Do you want it to be destroyed and a new one build? [y/n]"
+            read ANSWER
+            case "$ANSWER" in
+                y) echo "Destroying it and building a new one..."
+                   cd "$ANSIBLE_ROOT"
+                   echo "Removing the box..."
+                   ansible-playbook -vvvv clean.yml
+                   echo "Building a new box"
+                   ansible-playbook -vvvv prepare_test_box.yml
+                   break ;;
+
+                n) echo "Proceeding without building a new box..."
+                   break ;;
+
+                *) continue ;;
+            esac
+        done
     fi
 else
     echo "Building a new Ubuntu box..."
     cd "$ANSIBLE_ROOT"
-    ansible-playbook prepare_test_box.yml
+    ansible-playbook -vvvv prepare_test_box.yml
 fi
 
 cd "$ANSIBLE_ROOT"
-ansible-playbook apitests_prepare_zato.yml \
+echo "Preparing Zato quickstart environment..."
+ansible-playbook -vvvv apitests_prepare_zato.yml \
     --extra-vars "hostname=$HOSTNAME release_version=$RELEASE_VERSION
-                  package_version=$PACKAGE_VERSION distribution=$DISTRIBUTION" \
-    --user vagrant \
-    --private-key "$PRIVATE_KEY"
+                  package_version=$PACKAGE_VERSION distribution=$DISTRIBUTION
+                  repository=$REPOSITORY"
+
+echo "Running API tests..."
+ansible-playbook -vvvv run_apitests.yml \
+    --extra-vars "test_suite=$TEST_SUITE"
