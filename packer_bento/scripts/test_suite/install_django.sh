837dccc70346cd94701744736c3822739f0bf311
Rafał Krysiak <rafal@zato.io>
2016-01-21 15:12:57 +0100

    Removing unneeded files.

--- packer_bento/scripts/test_suite/install_django.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-# Install Django and additional apps
-pip install Django django-nose djangorestframework
--- packer_bento/scripts/test_suite/install_other_packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -eux
-
-# Install PyJWT
-sudo su - zato -c "/opt/zato/current/bin/pip install PyJWT"
