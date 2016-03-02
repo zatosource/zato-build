35da78438966afa7b101c4c150716875d08e6cb6
Rafał Krysiak <rafal@zato.io>
2016-03-02 18:35:11 +0100

    Removed 'cd' command.

--- docker/quickstart-dh/generate_password_web_admin.sh
+++ docker/quickstart-dh/generate_password_web_admin.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
 # Set a password for web admin and append it to a config file
-/opt/zato
 touch /opt/zato/web_admin_password
 uuidgen > /opt/zato/web_admin_password
 echo 'password'=$(cat /opt/zato/web_admin_password) >> /opt/zato/update_password.config
