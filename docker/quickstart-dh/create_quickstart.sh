33ee6f423962584394ca33f7aee526c57d09af79
Rafał Krysiak <rafal@zato.io>
2016-03-03 22:56:29 +0100

    Preparing a single script creating Zato quickstart and generating passwords.

--- docker/quickstart-dh/create_quickstart.sh
+++ docker/quickstart-dh/create_quickstart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eux
 
-sudo su - zato -c "rm -rf /opt/zato/env/qs-1 && mkdir -p /opt/zato/env/qs-1"
-cd /opt/zato/env/qs-1
-sudo su - zato -c "/opt/zato/current/bin/zato quickstart create . sqlite localhost 6379 --verbose --kvdb_password ''"
-sudo su - zato -c "sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' /opt/zato/env/qs-1/load-balancer/config/repo/zato.config"
-sudo su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server1/config/repo/server.conf"
-sudo su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server2/config/repo/server.conf"
+ZATO_ENV=/opt/zato/env/qs-1
+ZATO_BIN=/opt/zato/current/bin/zato
+
+su - zato -c "rm -rf $ZATO_ENV && mkdir -p $ZATO_ENV"
+su - zato -c "$ZATO_BIN quickstart create $ZATO_ENV sqlite localhost 6379 --verbose --kvdb_password ''"
+su - zato -c "sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' $ZATO_ENV/load-balancer/config/repo/zato.config"
+su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server1/config/repo/server.conf"
+su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server2/config/repo/server.conf"
