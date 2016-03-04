6925ba80c53a14fc350644b2a9a0b9e5ff6a2cba
Rafał Krysiak <rafal@zato.io>
2016-03-04 13:29:25 +0100

    Generate passwords in the same script which creates Zato quickstart.

--- docker/quickstart-dh/create_quickstart.sh
+++ docker/quickstart-dh/create_quickstart.sh
@@ -8,3 +8,16 @@ su - zato -c "$ZATO_BIN quickstart create $ZATO_ENV sqlite localhost 6379 --verb
 su - zato -c "sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' $ZATO_ENV/load-balancer/config/repo/zato.config"
 su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server1/config/repo/server.conf"
 su - zato -c "sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' $ZATO_ENV/server2/config/repo/server.conf"
+
+# Set a password for zato user
+su - zato "touch /opt/zato/zato_user_password /opt/zato/change_zato_password"
+su - zato "uuidgen > /opt/zato/zato_user_password"
+su - zato "chown zato:zato /opt/zato/zato_user_password"
+su - zato "echo 'zato':$(cat /opt/zato/zato_user_password) > /opt/zato/change_zato_password"
+chpasswd < /opt/zato/change_zato_password
+
+# Set a password for web admin and append it to a config file
+su - zato "touch /opt/zato/web_admin_password"
+su - zato "uuidgen > /opt/zato/web_admin_password"
+su - zato "echo 'password'=$(cat /opt/zato/web_admin_password) >> /opt/zato/update_password.config"
+su - zato "$ZATO_BIN from-config /opt/zato/update_password.config"
