08a3867f61badc127b1fd539e00d9ae9f7234382
Rafał Krysiak <rafal@zato.io>
2016-07-04 17:23:21 +0200

    Renamed the provision script and added web-admin password generator.

--- vagrant/zato-2.0.7-trusty64/setup-quickstart.sh
+++ vagrant/zato-2.0.7-trusty64/setup-quickstart.sh
@@ -10,3 +10,16 @@ ln -s /opt/zato/env/qs-1/load-balancer /etc/zato/components-enabled/load-balance
 ln -s /opt/zato/env/qs-1/server1 /etc/zato/components-enabled/server1
 ln -s /opt/zato/env/qs-1/server2 /etc/zato/components-enabled/server2
 ln -s /opt/zato/env/qs-1/web-admin /etc/zato/components-enabled/web-admin
+
+# Set a web-admin password
+touch /opt/zato/web-admin-password
+touch /opt/zato/update-password.config
+uuidgen > /opt/zato/web-admin-password
+echo '# Zato update password
+command=update_password
+path=/opt/zato/env/qs-1/web-admin
+store_config=True
+username=admin' >> /opt/zato/update-password.config
+echo 'password'=$(cat /opt/zato/web-admin-password) >> /opt/zato/update-password.config
+cd /opt/zato
+sudo su - zato -c "zato from-config /opt/zato/update-password.config"
