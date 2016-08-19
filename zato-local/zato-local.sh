43e927c51c4076d3eb32d5f7fa7fb8d6a686004c
Rafał Krysiak <rafal@zato.io>
2016-08-19 12:46:35 +0200

    Removed certificate generator; starting the Portal with gunicorn.

--- zato-local/zato-local.sh
+++ zato-local/zato-local.sh
@@ -26,26 +26,6 @@ check_exit_code
 source $HOME/.bashrc
 source $HOME/.bash_profile
 
-echo "Prepare SSL certificate for the Portal..."
-mkdir $HOME/tls
-cd $HOME/tls
-echo "Generating CA..."
-openssl genrsa -out portal.ca.key.pem 2048
-openssl req -new -x509 -days 3650 -extensions v3_ca \
-    -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Sample Dev CA" \
-    -key portal.ca.key.pem -out ca_cert.pem
-echo "Generating certificate for the Portal..."
-openssl genrsa -out portal.key.pem 2048
-openssl rsa -in portal.key.pem -pubout -out portal.key.pub.pem
-openssl req -new -key portal.key.pem -out portal.req.csr \
-    -subj "/C=EU/ST=Someland/L=Some City/O=Sample/CN=Portal"
-openssl x509 -req -days 365 -in portal.req.csr -CA ca_cert.pem -CAkey portal.ca.key.pem \
-    -CAcreateserial -out portal.cert.pem
-rm -f portal.req.csr
-echo "Concatenating Portal's cert with private key..."
-cat portal.cert.pem portal.key.pem > portal.pem
-cd $HOME
-
 echo "Starting redis-server..."
 redis-server --daemonize yes
 check_exit_code
@@ -109,16 +89,16 @@ zato enmasse $HOME/env/qs-1/server1 --input ./zato-export-*.json \
     --import --replace-odb-objects
 check_exit_code
 
-#echo "Starting gunicorn..."
-#cd $HOME/foxway.foxwayops
-#gunicorn foxwayid:app --daemon --log-file=portal.log --log-level DEBUG
-#check_exit_code
-
-echo "Starting the Portal as standalone Flask application..."
+echo "Starting gunicorn..."
 cd $HOME/foxway.foxwayops
-virtualenv registrationweb
-source registrationweb/bin/activate
-pip install -r requirements.txt
-pip uninstall pyScss --yes
-pip install pyScss
-python wsgi.py
+gunicorn foxwayid:app --daemon --log-file=portal.log
+check_exit_code
+
+#echo "Starting the Portal as standalone Flask application..."
+#cd $HOME/foxway.foxwayops
+#virtualenv registrationweb
+#source registrationweb/bin/activate
+#pip install -r requirements.txt
+#pip uninstall pyScss --yes
+#pip install pyScss
+#python wsgi.py
