61f84152eef4accc97f9d3d8a95bc519b530f165
Rafał Krysiak <rafal@zato.io>
2016-09-06 20:23:45 +0200

    Adding a few comments to the script.

--- ansible/create-env.sh
+++ ansible/create-env.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+echo "Cloning zato-build repository..."
+sleep 1
 git clone https://github.com/zatosource/zato-build.git
+echo ""
+sleep 1
+
+echo "Switching to u/ddmx branch..."
+sleep 1
 cd ./zato-build
 git fetch origin u/ddmx
 git checkout u/ddmx
+echo ""
+sleep 1
+
+echo "Creating an initial Zato distributed environment..."
+sleep 1
 cd ansible
 ansible-playbook -i environments/ddmx d_create_initial_env.yml \
     --extra-vars "odb_type=postgresql"
