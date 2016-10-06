9bbd3bcf1431d8a33752c821eef96cb88b1d3bca
Rafał Krysiak <rafal@zato.io>
2016-10-06 18:48:51 +0200

    Added a snippet which downloads Vagrant boxes and another one which sets up test repo boxes.

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ZATO_BUILD_ROOT=/opt/zato-build
+ANSIBLE_ROOT=$ZATO_BUILD_ROOT/ansible
+JENKINS_ROOT=/var/lib/jenkins
 VAGRANT_VERSION=1.8.6
 VB_VERSION=5.1
 
@@ -30,12 +33,42 @@ apt-get update
 apt-get install jenkins -y
 
 # Set up the jobs
-cd /opt/
-git clone https://github.com/zatosource/zato-build.git
-cd /opt/zato-build
+mkdir $ZATO_BUILD_ROOT
+cd $ZATO_BUILD_ROOT
+git clone https://github.com/zatosource/zato-build.git .
 git fetch origin jenkins
-cd /opt/zato-build/jenkins/ansible/jobs
+cd $ZATO_BUILD_ROOT/jenkins/ansible/jobs
 git checkout origin/jenkins
-cp -r ./* /var/lib/jenkins/jobs/
-chown -R jenkins:jenkins /var/lib/jenkins/jobs/*
-chown -R jenkins:jenkins /opt/zato-build/ansible
+cp -r ./* $JENKINS_ROOT/jobs/
+chown -R jenkins:jenkins $JENKINS_ROOT/jobs/*
+chown -R jenkins:jenkins $ANSIBLE_ROOT
+
+# Download Vagrant boxes
+VAGRANT_BOXES=(
+    bento/centos-6.7
+    bento/centos-6.7-i386
+    bento/debian-7.8
+    bento/debian-7.8-i386
+    bento/debian-7.9
+    bento/debian-7.9-i386
+    bento/debian-8.4
+    bento/debian-8.4-i386
+    bento/ubuntu-12.04
+    bento/ubuntu-12.04-i386
+    bento/ubuntu-14.04
+    bento/ubuntu-14.04-i386
+    bento/ubuntu-16.04
+    bento/ubuntu-16.04-i386
+)
+
+for box in ${VAGRANT_BOXES[@]}
+do
+    vagrant box add $box --provider virtualbox
+done
+
+# Setup test repo boxes
+REPO_BOXES=( repo-box-ubuntu repo-box-centos )
+for box in ${REPO_BOXES[@]}
+do
+    su - jenkins -c "cd $ANSIBLE_ROOT/vm/$box && vagrant up && vagrant halt"
+done
