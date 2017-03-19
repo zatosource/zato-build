2decefcd13e974bd3bed660aefa1dc0ebea09719
Rafal Krysiak <rafal@zato.io>
2017-03-20 00:32:50 +0100

    Rewritten the main function and added output for 'running' and 'aborter' VMs - initial functionality yet to be cleaned of redundant code.

--- ansible/files/check_vms.py
+++ ansible/files/check_vms.py
@@ -34,10 +34,10 @@ def format_info(box_name, box_id, box_state):
     box state: %s\n""" % (delimiter, box_name, box_id, box_state)
     return info
 
-def analyze_vms():
+def main():
     vms = {}
-    running_vms = []
-    aborted_vms = []
+    running_vms = {}
+    aborted_vms = {}
     idx = 1
 
     for box in box_list:
@@ -49,45 +49,63 @@ def analyze_vms():
 
         if len(box) > 2:
             box_name = " ".join(box[0:-1])
-        if "running" in box_state:
-            running_vms.append([box_name, box_id])
-        elif "aborted" in box_state:
-            aborted_vms.append([box_name, box_id])
 
-        info = format_info(box_name, box_id, box_state)
-        print(info)
+        if "running" in box_state:
+            running_vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
+        elif "aborted" in box_state:
+            aborted_vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
 
         vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
         idx += 1
 
-    return (vms, running_vms, aborted_vms)
-
-def main():
-
     if args.state == "all":
         header = (70 * "=") + "\n\n" + (4 * " ") + "List of VirtualBox VMs:\n"
         print(header)
-
-        analyzed = analyze_vms()
-        all_vms = len(analyzed[0])
-        running_vms = len(analyzed[1])
-        aborted_vms = len(analyzed[2])
-
-        footer = (70 * "=") + '\n'
-        print(footer)
-
-        print("Total number of VMs: %s" % (all_vms))
-        print("Number of running VMs: %s" % running_vms)
-        print("Number of aborted VMs: %s" % aborted_vms)
-        print("")
+        for i in vms:
+            name = vms[i]['name']
+            id = vms[i]['id']
+            state = vms[i]['state']
+            info = format_info(name, id, state)
+            print(info)
 
     elif args.state == "running":
         header = (70 * "=") + "\n\n" + (4 * " ") + "List of running VirtualBox VMs:\n"
         print(header)
+        if len(running_vms) == 0:
+            print(4 * " " + "No running VMs." + "\n")
+        else:
+            for i in running_vms:
+                name = running_vms[i]['name']
+                id = running_vms[i]['id']
+                state = running_vms[i]['state']
+                info = format_info(name, id, state)
+                print(info)
 
     elif args.state == "aborted":
         header = (70 * "=") + "\n\n" + (4 * " ") + "List of aborted VirtualBox VMs:\n"
         print(header)
+        if len(aborted_vms) == 0:
+            print(4 * " " + "No aborted VMs." + "\n")
+        else:
+            for i in aborted_vms:
+                name = aborted_vms[i]['name']
+                id = aborted_vms[i]['id']
+                state = aborted_vms[i]['state']
+                info = format_info(name, id, state)
+                print(info)
+
+    vms_qty = len(vms)
+    running_qty = len(running_vms)
+    aborted_qty = len(aborted_vms)
+
+    footer = (70 * "=") + '\n'
+
+    print(70 * "-" + "\n")
+    print(4 * " " + "Total number of VMs: %s" % (vms_qty))
+    print(4 * " " + "Number of running VMs: %s" % running_qty)
+    print(4 * " " + "Number of aborted VMs: %s" % aborted_qty)
+    print("")
+    print(footer)
 
 def running():
     pass
