9b0fcb22243a273bec558eef4e69e880381d4d24
Rafal Krysiak <rafal@zato.io>
2017-03-22 19:01:45 +0100

    Don't create repo directories for Zato 2.0.

--- ansible/vm/sign-rpm/files/repo.sh
+++ ansible/vm/sign-rpm/files/repo.sh
@@ -20,23 +20,3 @@ yum -y install expect
 
 # Start rng daemon
 service rngd start
-
-# Prepare repo directory structure
-if [ ! -f /var/www/repo ]
-then
-    echo 'Creating repo directory structure'
-    mkdir -p /var/www/repo/stable/2.0/rhel/el6/i386 \
-             /var/www/repo/stable/2.0/rhel/el6/x86_64 \
-             /var/www/repo/stable/2.0/rhel/el7/x86_64
-else
-    echo 'Directory structure is already there.'
-fi
-
-# Create repositories
-repo_root=/var/www/repo/stable/2.0/rhel
-repo_dirs=($repo_root/el6/i386 $repo_root/el6/x86_64 \
-           $repo_root/el7/x86_64)
-for dir in ${repo_dirs[*]}
-do
-    createrepo $dir
-done
