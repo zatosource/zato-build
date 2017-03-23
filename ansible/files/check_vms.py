42560ae3ae57cc00b0df1d66b0f29c4c08215ff6
Rafal Krysiak <rafal@zato.io>
2017-03-23 23:47:27 +0100

    Rewritten two functions, removed some redundacy and renamed most of variables which now have explicitly VB as a context.

--- ansible/files/check_vms.py
+++ ansible/files/check_vms.py
@@ -10,76 +10,60 @@ parser.add_argument("list")
 parser.add_argument("state", nargs="?", default="all")
 args = parser.parse_args()
 
-box_list = subprocess.check_output(["vboxmanage", "list", "vms"]).split("\n")
-# Remove exit code from box_list
-del box_list[-1]
+vm_list = subprocess.check_output(["vboxmanage", "list", "vms"]).split("\n")
+# Remove exit code from vm_list
+del vm_list[-1]
 
-def check_box_info(box):
-    box_info = subprocess.check_output(["vboxmanage", "showvminfo", box,])
-    return box_info
+def check_vm_info(box):
+    vm_info = subprocess.check_output(["vboxmanage", "showvminfo", box,])
+    return vm_info
 
-def get_box_state(box_info):
-    for item in box_info:
+def get_vm_state(vm_info):
+    for item in vm_info:
         m = re.match("State:\s+", item)
         if m:
             m = m.group(0)
-            box_state = item.replace(m, '')
-    return box_state
+            vm_state = item.replace(m, '')
+    return vm_state
 
-def get_info(vms, state):
-    header = (70 * "=") + "\n\n" + (4 * " ") + \
-        ("List of %s VirtualBox VMs:\n" % (state))
-    print(header)
-    delimiter = 70 * "-"
-    for i in vms:
-        name = vms[i]['name']
-        id = vms[i]['id']
-        state = vms[i]['state']
-        info = """%s\n
-    box name: %s\n
-    box id: %s\n
-    box state: %s\n""" % (delimiter, name, id, state)
-        print(info)
-
-
-def main():
-    vms = {}
-    running_vms = {}
-    aborted_vms = {}
-    idx = 1
-
-    for box in box_list:
-        box = box.split()
-        box_name = box[0].strip('"')
-        box_id = box[-1].strip("{}")
-        box_info = check_box_info(box_id).split('\n')
-        box_state = get_box_state(box_info)
-
-        if len(box) > 2:
-            box_name = " ".join(box[0:-1])
-
-        if "running" in box_state:
-            running_vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
-        elif "aborted" in box_state:
-            aborted_vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
-
-        vms[idx] = {'name': box_name, 'id': box_id, 'state': box_state}
-        idx += 1
-
-    if args.state == "all":
-        get_info(vms, args.state)
-
-    elif args.state == "running":
-        get_info(running_vms, args.state)
-
-    elif args.state == "aborted":
-        get_info(aborted_vms, args.state)
+def get_info(*args):
+    vms = args[0]
+    running_vms = args[1]
+    aborted_vms = args[2]
+    state = args[3]
 
     vms_qty = len(vms)
     running_qty = len(running_vms)
     aborted_qty = len(aborted_vms)
 
+    header = (70 * "=") + "\n\n" + (4 * " ") + \
+        ("List of %s VirtualBox VMs:\n" % (state))
+    delimiter = 70 * "-"
     footer = (70 * "=") + '\n'
+
+    print(header)
+
+    if state == "all":
+        current_vms = vms
+    elif state == "running":
+        current_vms = running_vms
+    elif state == "aborted":
+        current_vms = aborted_vms
+
+    if len(current_vms) == 0:
+        print("    There are %s VMs on this machine.\n" % (state))
+    elif len(current_vms) == 0 and state == "all":
+        print("    There are no VMs created on this machine.\n")
+
+    for vm in current_vms:
+        name = current_vms[vm]['name']
+        id = current_vms[vm]['id']
+        state = current_vms[vm]['state']
+        info = """%s\n
+    box name: %s\n
+    box id: %s\n
+    box state: %s\n""" % (delimiter, name, id, state)
+        print(info)
 
     print(70 * "-" + "\n")
     print(4 * " " + "Total number of VMs: %s" % (vms_qty))
@@ -87,5 +71,31 @@ def main():
     print(4 * " " + "Number of aborted VMs: %s" % aborted_qty)
     print("")
     print(footer)
+
+def main():
+    vms = {}
+    running_vms = {}
+    aborted_vms = {}
+    idx = 1
+
+    for vm in vm_list:
+        vm = vm.split()
+        vm_name = vm[0].strip('"')
+        vm_id = vm[-1].strip("{}")
+        vm_info = check_vm_info(vm_id).split('\n')
+        vm_state = get_vm_state(vm_info)
+
+        if len(vm) > 2:
+            vm_name = " ".join(vm[0:-1])
+
+        if "running" in vm_state:
+            running_vms[idx] = {'name': vm_name, 'id': vm_id, 'state': vm_state}
+        elif "aborted" in vm_state:
+            aborted_vms[idx] = {'name': vm_name, 'id': vm_id, 'state': vm_state}
+
+        vms[idx] = {'name': vm_name, 'id': vm_id, 'state': vm_state}
+        idx += 1
+
+    get_info(vms, running_vms, aborted_vms, args.state)
 
 main()
