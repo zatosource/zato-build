0bad2fc4bfd93959d57fd6ffb4262476979e8143
Dariusz Suchojad <dsuch-github@m.zato.io>
2017-05-23 22:30:15 +0200

    Updated dependencies for RHEL 6.

--- rhel6/package-base/_install-fedora.sh
+++ rhel6/package-base/_install-fedora.sh
@@ -11,22 +11,16 @@ function symlink_py {
     ln -s `python -c 'import '${1}', os.path, sys; sys.stdout.write(os.path.dirname('${1}'.__file__))'` $CURDIR/zato_extra_paths
 }
 
-
-#bash $CURDIR/clean.sh
-rm -rf $CURDIR/develop-eggs
-rm -rf $CURDIR/downloads
-rm -rf $CURDIR/eggs
-rm -rf $CURDIR/.installed.cfg
-rm -rf $CURDIR/parts
-rm -rf $CURDIR/zato_extra_paths
-
 sudo yum -y install git bzr gcc-gfortran haproxy \
     gcc-c++ atlas-devel atlas blas-devel  \
-    bzip2 bzip2-devel libev libev-devel libffi libffi-devel \
-    libevent-devel libgfortran lapack-devel lapack \
-    libpqxx-devel libyaml-devel libxml2-devel libxslt-devel suitesparse \
-    openssl openssl-devel python-devel numpy python-pip \
-    scipy python-zdaemon swig uuid-devel uuid
+    bzip2 bzip2-devel libffi libffi-devel \
+    libevent-devel libev libev-devel libgfortran lapack-devel lapack \
+    libyaml-devel libxml2-devel libxslt-devel suitesparse \
+    openssl openssl-devel postgresql-devel python-devel numpy \
+    scipy swig uuid-devel uuid
+
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
 
 mkdir $CURDIR/zato_extra_paths
 
@@ -36,15 +30,15 @@ symlink_py 'scipy'
 export CYTHON=$CURDIR/bin/cython
 
 sudo pip install --upgrade pip
-sudo pip install distribute==0.6.49
+sudo pip install setuptools==35.0.1
 sudo pip install virtualenv==1.9.1
 sudo pip install zato-apitest
 
-#    virtualenv .
+virtualenv --no-pip --python=$CURDIR/bin/python2.7 .
 
-
-$CURDIR/bin/python bootstrap.py -v 1.7.0
+$CURDIR/bin/python bootstrap.py
 $CURDIR/bin/buildout
 
 echo
 echo OK
+
