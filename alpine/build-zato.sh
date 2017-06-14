c05d7424361434fba985015a2691b31bf7083389
Laurent Bercot <ska-devel@skarnet.org>
2017-06-14 20:13:26 +0200

    GH #34 alpine: change Zato suffix versioning scheme
     ./build-zato.sh skarnet-f-gh762-alpine-linux-support 3.0.0
     ./build-zato.sh skarnet-f-gh762-alpine-linux-support 3.0.0 stable
     ./build-zato.sh skarnet-f-gh762-alpine-linux-support 3.0.0 pre0
     ./build-zato.sh skarnet-f-gh762-alpine-linux-support 3.0.0 rc1
    should all work. Valid third arguments are the Gentoo/Alpine
    accepted, suffixes, plus "stable" (which is equivalent to no suffix).
    
     Also added redis in makedepends, to pass the test suite; and
    documented build-zato.sh a bit.
    
     This commit should work nominally and now needs to be tested in-place.

--- alpine/build-zato.sh
+++ alpine/build-zato.sh
@@ -1,20 +1,47 @@
 #!/bin/sh -e
 
-test "$#" -lt 3 && { echo "build-zato.sh: usage: ./build-zato.sh branch-name zato-version package-version" 1>&2 ; exit 100 ; }
-test -z "$HOME" && { echo "build-zato.sh: HOME not set; abuild needs to run as a user with a home directory!" 1>&2 ; exit 100 ; }
+# This script will build the desired apk file in the directory it resides.
+# The produced apk file should then be added to the remote Alpine
+# repository and the index should be recomputed and re-signed; the
+# instructions to do so are available at
+# https://gitlab.com/zatosource/alpine-linux/wikis/how-to-manage-an-alpine-linux-repository
+
+
+test "$#" -ge 2 || { echo "build-zato.sh: usage: ./build-zato.sh branch-name zato-version [ package-version ]" 1>&2 ; exit 100 ; }
+test -n "$HOME" || { echo "build-zato.sh: HOME not set; abuild needs to run as a user with a home directory!" 1>&2 ; exit 100 ; }
 
 BRANCH_NAME="$1"
 ZATO_VERSION="$2"
+
+# The acceptable values for package-version are:
+# * nothing or "stable", for stable versions. (The Alpine version will then have no suffix.)
+# * "alpha", "beta", "pre" or "rc" followed by one or more digits.
+# This is a versioning convention coming from Gentoo, that Alpine also follows.
+
 PACKAGE_VERSION="$3"
+
+if test -z "${PACKAGE_VERSION}" || test "${PACKAGE_VERSION}" = "stable" ; then
+  COMPLETE_VERSION="${ZATO_VERSION}"
+else
+  COMPLETE_VERSION="${ZATO_VERSION}_${PACKAGE_VERSION}"
+fi
+
+apk version --check --quiet "${COMPLETE_VERSION}" || { echo "build-zato.sh: version $COMPLETE_VERSION is not valid for apk: suffixes must be {alpha|beta|pre|rc}[0-9]+" 1>&2 ; exit 100 ; }
+
 
 # This is the file where the packager's private key (to sign the apk)
 # is stored. The public key must be in the same place, with a ".rsa.pub"
 # suffix.
+
 # PACKAGER_PRIVKEY="$HOME/.abuild/dsuch@zato.io-XXXXXXXX.rsa"
 PACKAGER_PRIVKEY="$HOME/.abuild/ska-devel@skarnet.org-56139463.rsa"
 
+
+# Where we get Alpine from, and what version
+
 PREFERRED_REPOSITORY=http://dl-cdn.alpinelinux.org/alpine
 ALPINE_FLAVOUR=v3.6
+
 
 # These directories must be absolute.
 # DO NOT use /opt/zato for ZATO_ROOT_DIR. /opt is supposed to be
@@ -24,12 +51,10 @@ ALPINE_FLAVOUR=v3.6
 
 ZATO_ROOT_DIR=/pkg/zato
 ZATO_TARGET_DIR="$ZATO_ROOT_DIR/$ZATO_VERSION"
-COMPLETE_VERSION="${ZATO_VERSION}_${PACKAGE_VERSION}"
 
 CURDIR=`readlink -f .`
-
 sudo mkdir -p "$ZATO_TARGET_DIR"
-test "$ZATO_TARGET_DIR" != `readlink -f "$ZATO_TARGET_DIR"` && { echo "build-zato.sh: $ZATO_TARGET_DIR must be a fully resolved path!" 1>&2 ; exit 100 ; }
+test "$ZATO_TARGET_DIR" = `readlink -f "$ZATO_TARGET_DIR"` || { echo "build-zato.sh: $ZATO_TARGET_DIR must be a fully resolved path!" 1>&2 ; exit 100 ; }
 
 echo "Building zato-$COMPLETE_VERSION-r0.apk"
 
@@ -39,6 +64,7 @@ TARGETS="zato-$COMPLETE_VERSION.tar bash-completion $ABUILD_FILES"
 prepare_abuild() {
 
 # Ensure abuild can access the packager's keypair
+
   mkdir -p "$HOME/.abuild"
   echo "PACKAGER_PRIVKEY=$PACKAGER_PRIVKEY" > "$HOME/.abuild/abuild.conf"
   pubkey=`basename $PACKAGER_PRIVKEY`.pub
@@ -48,9 +74,11 @@ prepare_abuild() {
     sudo cp -f ${PACKAGER_PRIVKEY}.pub /etc/apk/keys
   fi
 
+
 # We need to pull py-numpy and py-numpy-f2py from community, and py-scipy
 # from testing. Testing is only available from edge. It's simpler to
 # rewrite our /etc/apk/repositories entirely and let abuild handle this.
+
   sudo sh -c "cat > /etc/apk/repositories.new && cp -f /etc/apk/repositories /etc/apk/repositories.old && mv -f /etc/apk/repositories.new /etc/apk/repositories && apk update && apk add tar alpine-sdk" <<EOF
 $PREFERRED_REPOSITORY/$ALPINE_FLAVOUR/main
 $PREFERRED_REPOSITORY/$ALPINE_FLAVOUR/community
@@ -102,10 +130,12 @@ call_abuild() {
   cd ..
 }
 
+
 isolate_package() {
   mv "$CURDIR/alpine/x86_64/zato-$COMPLETE_VERSION-r0.apk" "$CURDIR"
   rm -rf "$CURDIR/alpine"
 }
+
 
 prepare_abuild
 cleanup
