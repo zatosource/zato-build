e1595136af19152ab27083ccc8244a92c4022ee4
Dariusz Suchojad <dsuch-github@m.zato.io>
2018-06-25 18:44:41 +0200

    Don't install Python 2.7 twice (here and in _install-rhel.sh).

--- rhel6/build-zato.sh
+++ rhel6/build-zato.sh
@@ -36,11 +36,6 @@ ARCH=`uname -i`
 ZATO_ROOT_DIR=/opt/zato
 ZATO_TARGET_DIR=$ZATO_ROOT_DIR/$ZATO_VERSION
 
-PYTHON_VERSION=2.7.15
-PYTHON_ARCH_EXTENSION=tgz
-PYTHON_SRC_DIR=$TMP_DIR/Python-$PYTHON_VERSION
-PYTHON_BUILD_DIR=$CURDIR/python-build
-
 echo Building RHEL RPM zato-$ZATO_VERSION-$PACKAGE_VERSION.$RHEL_VERSION.$ARCH
 
 function prepare {
@@ -51,25 +46,7 @@ function prepare {
 function cleanup {
     rm -rf $TMP_DIR
     rm -rf $ZATO_TARGET_DIR
-    rm -rf $PYTHON_BUILD_DIR
     rm -rf $RPM_BUILD_DIR/BUILDROOT
-}
-
-function download_python {
-    mkdir -p $TMP_DIR
-    cd $TMP_DIR
-    wget -c https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.$PYTHON_ARCH_EXTENSION
-    tar -xvf ./Python-$PYTHON_VERSION.$PYTHON_ARCH_EXTENSION &> /dev/null
-}
-
-function install_python {
-    cd $TMP_DIR
-    $PYTHON_SRC_DIR/configure --prefix=$ZATO_TARGET_DIR/code
-    make -f $TMP_DIR/Makefile && make -f $TMP_DIR/Makefile altinstall
-
-    ln -s $ZATO_TARGET_DIR/code/bin/python2.7 $ZATO_TARGET_DIR/code/bin/python2
-    ln -s $ZATO_TARGET_DIR/code/bin/python2.7 $ZATO_TARGET_DIR/code/bin/python
-    strip -s $ZATO_TARGET_DIR/code/bin/python2.7
 }
 
 function checkout_zato {
@@ -121,8 +98,6 @@ function build_rpm {
 
 prepare
 cleanup
-download_python
 checkout_zato
-install_python
 install_zato
 build_rpm
