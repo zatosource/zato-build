111975c52a2b0ce2c53ba587b36d3a854f0101e1
Anielkis Herrera <aherrera@zato.io>
2019-03-13 21:36:58 -0500

    GH #42 - Ansible changes: Zato's quickstart updates

---
+++ vagrant/zato-3.0/provisioning/files/provisioning/quickstart-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /etc/environment
+
+cd /opt/zato/env/qs-1 || exit 1
+set -x # enable show commands
+/opt/zato/current/bin/zato quickstart create --odb_host localhost --odb_user postgres --odb_db_name zato --kvdb_password '' /opt/zato/env/qs-1/ postgresql localhost 6379
+/opt/zato/current/bin/zato from-config /opt/zato/update_password.config || exit 1
+sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' /opt/zato/env/qs-1/load-balancer/config/repo/zato.config || exit 1
+sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server1/config/repo/server.conf || exit 1
+sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server2/config/repo/server.conf || exit 1
+set +x # disable show commands
