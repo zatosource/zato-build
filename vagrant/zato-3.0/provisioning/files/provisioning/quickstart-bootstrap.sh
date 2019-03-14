28f3ad17260f7b0e5c3b855d0fa3e73a0653c7b2
Anielkis Herrera <aherrera@zato.io>
2019-03-14 14:16:04 -0500

    GH #42 - Ansible changes: PostgreSQL initialization. Still some issues with Zato's quickstart

--- vagrant/zato-3.0/provisioning/files/provisioning/quickstart-bootstrap.sh
+++ vagrant/zato-3.0/provisioning/files/provisioning/quickstart-bootstrap.sh
@@ -3,8 +3,9 @@ source /etc/environment
 
 cd /opt/zato/env/qs-1 || exit 1
 set -x # enable show commands
-/opt/zato/current/bin/zato quickstart create --odb_host localhost --odb_user postgres --odb_db_name zato --kvdb_password '' /opt/zato/env/qs-1/ postgresql localhost 6379
-/opt/zato/current/bin/zato from-config /opt/zato/update_password.config || exit 1
+# /opt/zato/current/bin/zato delete odb --verbose --odb_host localhost --odb_user zato --odb_password mysupersecretpassword --odb_db_name zato postgresql
+/opt/zato/current/bin/zato quickstart create --verbose --odb_host localhost --odb_user zato --odb_password mysupersecretpassword --odb_db_name zato --kvdb_password '' /opt/zato/env/qs-1/ postgresql localhost 6379
+/opt/zato/current/bin/zato from-config --verbose /opt/zato/update_password.config || exit 1
 sed -i 's/127.0.0.1:11223/0.0.0.0:11223/g' /opt/zato/env/qs-1/load-balancer/config/repo/zato.config || exit 1
 sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server1/config/repo/server.conf || exit 1
 sed -i 's/gunicorn_workers=2/gunicorn_workers=1/g' /opt/zato/env/qs-1/server2/config/repo/server.conf || exit 1
