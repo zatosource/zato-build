a1d50ed1756ad8659453595ca90c4b33787cb936
Anielkis Herrera <aherrera@zato.io>
2019-03-20 12:00:30 -0500

    GH #44 - Adding password update process for ide_publisher: adding exit codes

--- docker/quickstart/ide_password_update.sh
+++ docker/quickstart/ide_password_update.sh
@@ -4,13 +4,21 @@ source /etc/environment
 cd /opt/zato/env/qs-1 || exit 1
 
 if [[ -n "${ZATO_IDE_PUBLISHER_PASSWORD}" ]]; then
-    wget -q -O /opt/zato/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x /opt/zato/jq
+    if [[ ! -x /opt/zato/jq  ]]; then
+        wget -q -O /opt/zato/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x /opt/zato/jq
+    fi
+
     # Wait for server1 to start
     /usr/local/bin/dockerize -wait-http-status-code 200 -wait-retry-interval 10s -wait http://localhost:17010/zato/ping -timeout 10m
 
     echo "Updating ide_publisher password"
     user_id="$(curl -q "http://admin:$(cat ~/web_admin_password)@localhost:11223/zato/api/invoke/zato.security.basic-auth.get-list?cluster_id=1" 2>/dev/null | /opt/zato/jq '.[] | select(.username == "ide_publisher").id')"
+    if [[ -z "${user_id}" ]]; then
+        echo "Failed to get user id"
+        exit 1
+    fi
     $ZATO_BIN service invoke --verbose --payload \
         "{\"password1\":\"${ZATO_IDE_PUBLISHER_PASSWORD}\", \"password2\": \"${ZATO_IDE_PUBLISHER_PASSWORD}\", \"id\": ${user_id}}" \
         /opt/zato/env/qs-1/server1/ zato.security.basic-auth.change-password || exit 1
+    echo "ide_publisher password updated"
 fi
