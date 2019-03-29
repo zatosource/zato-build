331efb96e302e029c3ef8395e9789a735406fd13
Anielkis Herrera <aherrera@zato.io>
2019-03-29 08:56:19 -0500

    GH #39 - Copying packages to folders to be used for testing

--- .travis/test_packaging.sh
+++ .travis/test_packaging.sh
@@ -13,6 +13,7 @@ case "$name" in
   alpine) repo="alpine" ; export ALPINE_FLAVOUR="v$version" ;;
 esac
 
-run bash -ec "cd /tmp/zato-build/$repo && exec ./build-zato.sh main $ZATO_VERSION $PACKAGE_VERSION ${PY_BINARY:-python}"
+run bash -ec "cd /tmp/zato-build/$repo && exec ./build-zato.sh main \"$ZATO_VERSION\" \"$PACKAGE_VERSION\" \"${PY_BINARY:-python}\""
 
+echo "Results"
 find $TRAVIS_BUILD_DIR/packages/
