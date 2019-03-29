72856cc00336d38d46e412cb38b96acf2615b229
Anielkis Herrera <aherrera@zato.io>
2019-03-29 08:26:38 -0500

    GH #39 - Copying packages to folders to be used for testing

--- deb/build-zato.sh
+++ deb/build-zato.sh
@@ -163,6 +163,9 @@ function build_deb {
     dpkg-deb --build zato-$ZATO_VERSION-$PACKAGE_VERSION\_$ARCH
 
     mv $CURDIR/BUILDROOT/zato-$ZATO_VERSION-$PACKAGE_VERSION\_$ARCH.deb $CURDIR/zato-$ZATO_VERSION-$PACKAGE_VERSION\_$ARCH-$RELEASE_NAME.deb
+
+    [[ -d "/tmp/zato-build/$(lsb_release -c | cut -f2)/" ]] || mkdir -p "/tmp/zato-build/$(lsb_release -c | cut -f2)/"
+    cp "$CURDIR/zato-$ZATO_VERSION-$PACKAGE_VERSION\_$ARCH-$RELEASE_NAME.deb" "/tmp/zato-build/$(lsb_release -c | cut -f2)/"
 }
 
 cleanup
