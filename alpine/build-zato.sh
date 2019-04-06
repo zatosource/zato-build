1fc0d66b04b8942a0e0027bea96467321d99af88
Anielkis Herrera <aherrera@zato.io>
2019-04-06 15:18:22 -0500

    GH #39 - Alpine Linux package dependencies updates

--- alpine/build-zato.sh
+++ alpine/build-zato.sh
@@ -162,6 +162,7 @@ checkout_and_make_archive() {
   done
   git checkout "$BRANCH_NAME"
   sed -i -e 's/lapack/openblas/' -e 's/ libtool/ libtool cython/' code/_install-alpine.sh
+  sed -i -e 's/numpy==.*/numpy==1.16.2/' -e 's/scipy==.*/scipy==1.2.1/' code/requirements.txt
   cd ..
   tar -cf "package-base/zato-$COMPLETE_VERSION.tar" "zato-$COMPLETE_VERSION"
   rm -rf "zato-$COMPLETE_VERSION"
