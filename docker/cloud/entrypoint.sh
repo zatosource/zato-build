36d6851c5b9f51f924ad9740881e695354d43a8e
Anielkis Herrera <aherrera@zato.io>
2019-04-17 11:41:28 -0500

    GH #46 - Updating Docker image it is using the main branch for updates

--- docker/cloud/entrypoint.sh
+++ docker/cloud/entrypoint.sh
@@ -39,7 +39,7 @@ if [[ -n "${ODB_HOSTNAME}" ]]; then
         echo "ODB_TYPE=\"${ODB_TYPE}\"" >> /etc/environment
     fi
     WAITS="${WAITS} -wait tcp://${ODB_HOSTNAME}:${ODB_PORT} -timeout 10m "
-    echo "ODB_DATA=\"--odb_host ${ODB_HOSTNAME} --odb_port ${ODB_PORT} --odb_user ${ODB_USERNAME} --odb_db_name ${ODB_NAME} --odb_password ${ODB_PASSWORD}\"" >> /etc/environment
+    ODB_DATA="--odb_host ${ODB_HOSTNAME} --odb_port ${ODB_PORT} --odb_user ${ODB_USERNAME} --odb_db_name ${ODB_NAME} --odb_password ${ODB_PASSWORD}"
 else
     echo "ODB_HOSTNAME can't be empty"
     exit 1
@@ -63,52 +63,49 @@ if [[ "$ZATO_POSITION" != "load-balancer" ]]; then
     sleep 30
 fi
 
+gosu zato bash -c  "${ZATO_BIN} --version"
+
 case "$ZATO_POSITION" in
     "load-balancer" )
-    if [[ -z ${REDIS_HOSTNAME} ]]; then
-        echo "REDIS_HOSTNAME not defined"
-        exit 1
-    fi
-    gosu zato bash -c "${ZATO_BIN} create odb ${ODB_DATA} ${ODB_TYPE}"
-    [[ -n "${ZATO_WEB_ADMIN_PASSWORD}" ]] && ZATO_WEB_ADMIN_PASSWORD="--tech_account_password ${ZATO_WEB_ADMIN_PASSWORD}"
-    gosu zato bash -c "${ZATO_BIN} create cluster ${ODB_DATA} ${ZATO_WEB_ADMIN_PASSWORD} ${ODB_TYPE} ${LB_HOST:-zato.localhost} ${LB_PORT:-11223} ${LB_AGENT_PORT:-20151} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME} ${TECH_ACCOUNT_NAME:-admin}"
-    gosu zato bash -c "${ZATO_BIN} create load_balancer /opt/zato/env/qs-1/load-balancer"
-    export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/load-balancer --fg"
+        if [[ -z ${REDIS_HOSTNAME} ]]; then
+            echo "REDIS_HOSTNAME not defined"
+            exit 1
+        fi
+        gosu zato bash -c "${ZATO_BIN} create odb ${ODB_DATA} ${ODB_TYPE}"
+        [[ -n "${ZATO_WEB_ADMIN_PASSWORD}" ]] && ZATO_WEB_ADMIN_PASSWORD="--tech_account_password ${ZATO_WEB_ADMIN_PASSWORD}"
+        gosu zato bash -c "${ZATO_BIN} create cluster ${ODB_DATA} ${ZATO_WEB_ADMIN_PASSWORD} ${ODB_TYPE} ${LB_HOST:-zato.localhost} ${LB_PORT:-11223} ${LB_AGENT_PORT:-20151} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME} ${TECH_ACCOUNT_NAME:-admin}"
+        gosu zato bash -c "${ZATO_BIN} create load_balancer /opt/zato/env/qs-1/load-balancer"
+        export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/load-balancer --fg"
     ;;
     "scheduler" )
-    if [[ -z ${REDIS_HOSTNAME} ]]; then
-        echo "REDIS_HOSTNAME not defined"
-        exit 1
-    fi
+        if [[ -z ${REDIS_HOSTNAME} ]]; then
+            echo "REDIS_HOSTNAME not defined"
+            exit 1
+        fi
 
-    [[ -n "${SECRET_KEY}" ]] && SECRET_KEY="--secret_key ${SECRET_KEY}"
+        [[ -n "${SECRET_KEY}" ]] && SECRET_KEY="--secret_key ${SECRET_KEY}"
 
-    gosu zato bash -c "${ZATO_BIN} create scheduler ${ODB_DATA} ${SECRET_KEY} /opt/zato/env/qs-1/scheduler ${ODB_TYPE} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME}"
+        gosu zato bash -c "${ZATO_BIN} create scheduler ${ODB_DATA} ${SECRET_KEY} /opt/zato/env/qs-1/scheduler ${ODB_TYPE} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME}"
 
-    export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/scheduler --fg"
+        export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/scheduler --fg"
     ;;
     "server" )
-    if [[ -z ${REDIS_HOSTNAME} ]]; then
-        echo "REDIS_HOSTNAME not defined"
-        exit 1
-    fi
+        if [[ -z ${REDIS_HOSTNAME} ]]; then
+            echo "REDIS_HOSTNAME not defined"
+            exit 1
+        fi
 
-    [[ -n "${JWT_SECRET_KEY}" ]] && JWT_SECRET_KEY="--jwt_secret ${JWT_SECRET_KEY}"
-    [[ -n "${SECRET_KEY}" ]] && SECRET_KEY="--secret_key ${SECRET_KEY}"
+        [[ -n "${JWT_SECRET_KEY}" ]] && JWT_SECRET_KEY="--jwt_secret ${JWT_SECRET_KEY}"
+        [[ -n "${SECRET_KEY}" ]] && SECRET_KEY="--secret_key ${SECRET_KEY}"
 
-    gosu zato bash -c "${ZATO_BIN} create server ${ODB_DATA} ${JWT_SECRET_KEY} ${SECRET_KEY} --http_port 17010 /opt/zato/env/qs-1/server ${ODB_TYPE} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME} ${SERVER_NAME}"
+        gosu zato bash -c "${ZATO_BIN} create server ${ODB_DATA} ${JWT_SECRET_KEY} ${SECRET_KEY} --http_port 17010 /opt/zato/env/qs-1/server ${ODB_TYPE} ${REDIS_HOSTNAME} ${REDIS_PORT:-6379} ${CLUSTER_NAME} ${SERVER_NAME}"
 
-    export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/server --fg"
+        export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/server --fg"
     ;;
     "webadmin" )
-    if [[ -z ${REDIS_HOSTNAME} ]]; then
-        echo "REDIS_HOSTNAME not defined"
-        exit 1
-    fi
-
-    [[ -n "${ZATO_WEB_ADMIN_PASSWORD}" ]] && ZATO_WEB_ADMIN_PASSWORD="--tech_account_password ${ZATO_WEB_ADMIN_PASSWORD}"
-    gosu zato bash -c "${ZATO_BIN} create web_admin ${ODB_DATA} ${ZATO_WEB_ADMIN_PASSWORD} /opt/zato/env/qs-1/webadmin ${ODB_TYPE} ${TECH_ACCOUNT_NAME:-admin}"
-    export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/webadmin --fg"
+        [[ -n "${ZATO_WEB_ADMIN_PASSWORD}" ]] && ZATO_WEB_ADMIN_PASSWORD="--tech_account_password ${ZATO_WEB_ADMIN_PASSWORD}"
+        gosu zato bash -c "${ZATO_BIN} create web_admin ${ODB_DATA} ${ZATO_WEB_ADMIN_PASSWORD} /opt/zato/env/qs-1/webadmin ${ODB_TYPE} ${TECH_ACCOUNT_NAME:-admin}"
+        export ZATO_SERVICE="${ZATO_BIN} start /opt/zato/env/qs-1/webadmin --fg"
     ;;
 esac
 
