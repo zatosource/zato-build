ce87207dc80ecf3dcaa4ced9d3fcd0451ab445cc
Anielkis Herrera <aherrera@zato.io>
2019-05-22 06:36:11 -0500

    GH #39 - dateparser version

--- deb/build-zato.sh
+++ deb/build-zato.sh
@@ -139,7 +139,8 @@ function checkout_zato {
 function install_zato {
 
     cd $ZATO_TARGET_DIR/code
-    sed -i -e 's|pg8000==1.13.1|pg8000==1.12.5|' requirements.txt
+    sed -i -e 's|pg8000==1.13.1|pg8000==1.12.5|' \
+           -e 's|dateparser==0.5.1|dateparser==0.7.1|' requirements.txt
     ./install.sh -p ${PY_BINARY}
 
     find $ZATO_TARGET_DIR/. -name *.pyc -exec rm -f {} \;
