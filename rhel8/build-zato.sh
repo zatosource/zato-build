b510c5cabefb7a2b291883c895a788479a5ce2d7
Anielkis Herrera <aherrera@zato.io>
2019-10-09 09:24:18 -0500

    GH #39 - Update RHEL8 python support

--- rhel8/build-zato.sh
+++ rhel8/build-zato.sh
@@ -126,6 +126,7 @@ function install_zato {
 function build_rpm {
     sudo yum install -y ${PY_BINARY:-python2}-devel
     rm -f $SOURCE_DIR/zato.spec
+    cat $SOURCE_DIR/zato.spec.template
     cp $SOURCE_DIR/zato.spec.template $SOURCE_DIR/zato.spec
     sed -i.bak "s/PYTHON_DEPS/${PY_BINARY}/g" $SOURCE_DIR/zato.spec
     sed -i.bak "s/ZATO_VERSION/$ZATO_VERSION/g" $SOURCE_DIR/zato.spec
