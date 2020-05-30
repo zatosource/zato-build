dd23c5e477c3dd9e73b1cac65afc40e096a3da0c
Anielkis Herrera <aherrera@zato.io>
2020-05-29 20:53:24 -0500

    Checking dependencies

--- rhel7/build-zato.sh
+++ rhel7/build-zato.sh
@@ -132,7 +132,7 @@ function checkout_zato {
 
 function install_zato {
     cd $ZATO_TARGET_DIR/code
-    sed -i -e 's|pg8000==1.13.1|pg8000==1.12.5|' requirements.txt
+    sed -i -e 's|pg8000==1.13.1|pg8000==1.12.5|' -e 's|pyasn1==0.4.5|pyasn1==0.4.8|' requirements.txt
     ./install.sh -p ${PY_BINARY}
     find $ZATO_TARGET_DIR/. -name *.pyc -exec rm -f {} \;
     find $ZATO_TARGET_DIR/. ! -perm /004 -exec chmod 644 {} \;
