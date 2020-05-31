8111e7d18515ed12fe7990a2f557381944d3e1e9
Anielkis Herrera <aherrera@zato.io>
2020-05-30 23:39:08 -0500

    Updating deb package

--- deb/build-zato.sh
+++ deb/build-zato.sh
@@ -204,6 +204,7 @@ function install_zato {
             -e 's|^cytoolz==.*|cytoolz==0.10.1|' \
             -e 's|cffi==.*|cffi==1.14.0|' \
             -e 's|lxml==.*|lxml==4.4.3|' \
+            -e 's|psycopg2==.*|psycopg2==2.8.5|' \
             _postinstall.sh \
             requirements.txt
     fi
