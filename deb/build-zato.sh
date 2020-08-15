f82c2732823652f3dd36ed045e91e56b9f6afd36
Anielkis Herrera <aherrera@zato.io>
2020-08-15 13:18:42 -0500

    bzr fails to install in Python 2.7:
        Collecting bzr==2.6.0
          Downloading bzr-2.6.0.tar.gz (11.3 MB)
            ERROR: Command errored out with exit status 1:
            Complete output (5 lines):
            Traceback (most recent call last):
              File "<string>", line 1, in <module>
              File "/tmp/pip-install-vVfVj5/bzr/setup.py", line 205, in <module>
                pyrex_version_info = tuple(map(int, pyrex_version.rstrip("+").split('.')))
            ValueError: invalid literal for int() with base 10: '0a5'
            ----------------------------------------
        ERROR: Command errored out with exit status 1: python setup.py egg_info Check the logs for full command output.

--- deb/build-zato.sh
+++ deb/build-zato.sh
@@ -168,6 +168,7 @@ function install_zato {
 
     cd $ZATO_TARGET_DIR/code
     sed -i -e 's|dateparser==0.5.1|dateparser==0.7.1|' -e 's|pyasn1==0.4.5|pyasn1==0.4.8|' requirements.txt
+    sed -i -e 's|bzr==2.6.0|bzr==2.7.0|' _req_py27.txt
     if [[ $(${PY_BINARY} -c 'import sys; print(sys.version_info[:][1])') -eq 4 ]]; then
         sed -i -e 's|pg8000==1.13.1|pg8000==1.12.3|' _req_py3.txt
     fi
